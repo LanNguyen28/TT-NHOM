a0848dd1e741212670a1a9c3727bc5df3ac12b6e
LanNguyen28 <61912463+LanNguyen28@users.noreply.github.com>
2020-04-04 22:36:29 +0700

    Login and Main

---
+++ QL_KHO/QL_Kho/FrmMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace QL_Kho
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnthongke_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void hELPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Process.Start("http://quanlykho.comeze.com/");
+        }
+    }
+}
