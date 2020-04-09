a4dc981fd90dff51e0cd71beafa6706d06de8e69
maingocle <63176236+maingocle@users.noreply.github.com>
2020-04-09 22:31:09 +0700

    Thống kê hàng hóa

--- Data/DAO.cs
+++ Data/DAO.cs
@@ -13,8 +13,31 @@ namespace QL_Kho.Data
     {
 
         //thống kê
-       
-        
+        public static DataTable tk_hh()
+        {
+            return Dataprovider.GetData("tk_hh");
+        }
+        public static DataTable tk_xuat()
+        {
+            return Dataprovider.GetData("tk_xuat");
+        }
+        public static DataTable tk_nhap()
+        {
+            return Dataprovider.GetData("tk_nhap");
+        }
+        //thêm
+        public static int them_CTN(ChiTietNhap a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maCTN", a.MaCTN),
+                new SqlParameter("@maPN", a.MaPN),
+                new SqlParameter("@maHH", a.MaHH),
+                new SqlParameter("@soLuong", a.SoLuong),
+                new SqlParameter("@donGia", a.DonGia),
+            };
+            return Dataprovider.ExecuteNonQuery("them_CTN", para);
+        }
         public static int them_CTX(ChiTietXuat a)
         {
             SqlParameter[] para = new SqlParameter[]
@@ -27,7 +50,39 @@ namespace QL_Kho.Data
             };
             return Dataprovider.ExecuteNonQuery("them_CTX", para);
         }
-      
+        public static int them_HH(HangHoa a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maHH", a.MaHH),
+                new SqlParameter("@tenHH", a.TenHH),
+                new SqlParameter("@maNCC", a.MaNCC),
+                new SqlParameter("@soLuong", a.SoLuong),
+            };
+            return Dataprovider.ExecuteNonQuery("them_HH", para);
+        }
+        public static int them_NCC(NCC a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maNCC", a.MaNCC),
+                new SqlParameter("@tenNCC", a.TenNCC),
+                new SqlParameter("@diaChi", a.DiaChi),
+                new SqlParameter("@SDT", a.SoDT),
+                new SqlParameter("@email", a.Email),
+            };
+            return Dataprovider.ExecuteNonQuery("them_NCC", para);
+        }
+        public static int them_PN(PhieuNhap a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maPN", a.MaPN),
+                new SqlParameter("@ngayNhap", a.NgayNhap),
+                new SqlParameter("@tongTien", a.TongTien),
+            };
+            return Dataprovider.ExecuteNonQuery("them_PN", para);
+        }
         public static int them_PX(PhieuXuat a)
         {
             SqlParameter[] para = new SqlParameter[]
@@ -39,7 +94,19 @@ namespace QL_Kho.Data
             return Dataprovider.ExecuteNonQuery("them_PX", para);
         }
         //sua
-        
+        public static int sua_CTN(ChiTietNhap a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maCTN", a.MaCTN),
+                new SqlParameter("@maPN", a.MaPN),
+                new SqlParameter("@maHH", a.MaHH),
+                new SqlParameter("@soLuong", a.SoLuong),
+                new SqlParameter("@donGia", a.DonGia),
+
+            };
+            return Dataprovider.ExecuteNonQuery("sua_CTN", para);
+        }
         public static int sua_CTX(ChiTietXuat a)
         {
             SqlParameter[] para = new SqlParameter[]
@@ -53,7 +120,38 @@ namespace QL_Kho.Data
             };
             return Dataprovider.ExecuteNonQuery("sua_CTX", para);
         }
-      
+        public static int sua_HH(HangHoa a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maHH", a.MaHH),
+                new SqlParameter("@tenHH", a.TenHH),
+                new SqlParameter("@maNCC", a.MaNCC),
+            };
+            return Dataprovider.ExecuteNonQuery("sua_HH", para);
+        }
+        public static int sua_NCC(NCC a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maNCC", a.MaNCC),
+                new SqlParameter("@tenNCC", a.TenNCC),
+                new SqlParameter("@diaChi", a.DiaChi),
+                new SqlParameter("@SDT", a.SoDT),
+                new SqlParameter("@email", a.Email),
+            };
+            return Dataprovider.ExecuteNonQuery("sua_NCC", para);
+        }
+        public static int sua_PN(PhieuNhap a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maPN", a.MaPN),
+                new SqlParameter("@ngayNhap", a.NgayNhap),
+                new SqlParameter("@tongTien", a.TongTien),
+            };
+            return Dataprovider.ExecuteNonQuery("sua_PN", para);
+        }
         public static int sua_PX(PhieuXuat a)
         {
             SqlParameter[] para = new SqlParameter[]
@@ -65,7 +163,16 @@ namespace QL_Kho.Data
             return Dataprovider.ExecuteNonQuery("sua_PX", para);
         }
         //xóa
-       
+        public static int xoa_CTN(ChiTietNhap a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maCTN", a.MaCTN),
+
+
+            };
+            return Dataprovider.ExecuteNonQuery("xoa_CTN", para);
+        }
         public static int xoa_CTX(ChiTietXuat a)
         {
             SqlParameter[] para = new SqlParameter[]
@@ -76,7 +183,36 @@ namespace QL_Kho.Data
             };
             return Dataprovider.ExecuteNonQuery("xoa_CTX", para);
         }
-        
+        public static int xoa_HH(HangHoa a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maHH", a.MaHH),
+
+
+            };
+            return Dataprovider.ExecuteNonQuery("xoa_HH", para);
+        }
+        public static int xoa_NCC(NCC a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maNCC", a.MaNCC),
+
+
+            };
+            return Dataprovider.ExecuteNonQuery("xoa_NCC", para);
+        }
+        public static int xoa_PN(PhieuNhap a)
+        {
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@maPN", a.MaPN),
+
+
+            };
+            return Dataprovider.ExecuteNonQuery("xoa_PN", para);
+        }
         public static int xoa_PX(PhieuXuat a)
         {
             SqlParameter[] para = new SqlParameter[]
@@ -88,7 +224,14 @@ namespace QL_Kho.Data
             return Dataprovider.ExecuteNonQuery("xoa_PX", para);
         }
         //xuat
-       
+        public static DataTable xuat_pn()
+        {
+            return Dataprovider.GetData("xuat_PN");
+        }
+        public static DataTable xuat_ctn(string ma)
+        {
+            return Dataprovider.GetData("xuat_CTN2" + "'" + ma + "'");
+        }
         public static DataTable xuat_px()
         {
             return Dataprovider.GetData("xuat_PX");
@@ -97,8 +240,17 @@ namespace QL_Kho.Data
         {
             return Dataprovider.GetData("xuat_CTX2" + "'" + ma + "'");
         }
-       
-        
-       
+        public static DataTable xuat_hh()
+        {
+            return Dataprovider.GetData("xuat_HH");
+        }
+        public static DataTable xuat_hh_tk()
+        {
+            return Dataprovider.GetData("xuat_hh_tk");
+        }
+        public static DataTable xuat_ncc()
+        {
+            return Dataprovider.GetData("xuat_NCC");
+        }
     }
 }
