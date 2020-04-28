ed760695db624dd5dc94fbe472a22861c2c8f470
LanNguyen28 <61912463+LanNguyen28@users.noreply.github.com>
2020-04-28 21:34:16 +0700

    File DT0
    ttongr hợp file

---
+++ DT0/NCC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_Kho.DT0
+{
+    class NCC
+    {
+        private string maNCC;
+        private string tenNCC;
+        private string diaChi;
+    
+
+        public string MaNCC
+        {
+            get
+            {
+                return maNCC;
+            }
+
+            set
+            {
+                maNCC = value;
+            }
+        }
+        public string TenNCC
+        {
+            get
+            {
+                return tenNCC;
+            }
+
+            set
+            {
+                tenNCC = value;
+            }
+        }
+        public string DiaChi
+        {
+            get
+            {
+                return diaChi;
+            }
+
+            set
+            {
+                diaChi = value;
+            }
+        }
+        
+    }
+}
